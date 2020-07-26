36c0fd4e08e1ef4d9f918a040da5e1909a98de51
kIvy1189 <kevindivy@gmail.com>
2020-07-26 18:52:39 -0500

    Added thought schema

--- models/User.js
+++ models/User.js
@@ -4,13 +4,13 @@ const UserSchema = new Schema(
     {
         username: {
             type: String,
-            required: 'Username is required',
+            required: true,
             unique: true,
             trim: true,
         },
         email: {
             type: String,
-            required: 'Email is required',
+            required: true,
             unique: true,
             match: [/^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z]{2,6})$/]
         },
